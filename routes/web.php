3f4857318c0b5b45e5b35cd75e17365ccf732dcf
Gabriel Marques <bielmarques922@gmail.com>
2019-12-17 10:12:45 -0300

    Parametros das rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { // Route é uma classe e o get é um metodo 
+    return view('welcome'); // Para listar as rotas e so colocar "$ php artisan route:list"
+});
+
+Route::get('/ola/{nome}/{sobrenome}', function($nome, $sobrenome){ // Pode se usar echo ou Return
+    return "Ola! Seja bem vindo, $nome $sobrenome!"; 
 });
