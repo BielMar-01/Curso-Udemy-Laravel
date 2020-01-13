3cc8d80bca61f0e92329d93773021f0e1ecab3f8
Gabriel Marques <bielmarques922@gmail.com>
2020-01-13 17:25:00 -0300

    Método Destroy

--- storage/framework/views/4ce48256c6658b37b717637190c1886c0bb08b2a.php
+++ storage/framework/views/4ce48256c6658b37b717637190c1886c0bb08b2a.php
@@ -7,6 +7,11 @@
             <?php echo e($c['nome']); ?> | 
             <a href="<?php echo e(route('clientes.edit', $c['id'] )); ?>">Editar</a> |
             <a href="<?php echo e(route('clientes.show', $c['id'] )); ?>">Info</a>
+            <form action="<?php echo e(route('clientes.destroy', $c['id'])); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <?php echo method_field('DELETE'); ?>
+                <input type="submit" value="Apagar">
+            </form>
         </li>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 </ol><?php /**PATH /home/gabriel/code/projetoUdemy/resources/views/clientes/index.blade.php ENDPATH**/ ?>
